002f425cfecaf288ceed1ae1bd5197d5ce5e6d0b
SpaceNoob123 <markusgame31@gmail.com>
2023-03-31 11:20:08 +0300

    Created

---
+++ tic-tac-toe/Form1.Designer.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace tic_tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.Text = "Tic-Tac-Toe";
+
+        }
+        #endregion
+    }
+}
+
